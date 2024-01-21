c175b7f3db494a4535688e294336a0b2e169e8e6
l0rdyolo <wp.omer820@gmail.com>
2024-01-21 23:51:02 +0300

    little

--- The Random Walker/Walker.js
+++ The Random Walker/Walker.js
@@ -1,6 +1,30 @@
 class Walker{
-    constructor(){
-        this.x = width/2;
-        this.x = height/2;
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        console.log(this.x,this.y);
+        this.choice = 0;
+        this.r = random(255)
+        this.g = random(255)
+        this.b = random(255)
+
+    }
+
+    show(){
+        stroke(this.r ,this.g,this.b)
+        point(this.x, this.y)
+    }
+
+    step(){
+        this.choice = floor(random(4));
+        if (this.choice === 0) {
+            this.x++;
+        } else if (this.choice === 1) {
+            this.x--;
+        } else if (this.choice === 2) {
+            this.y++;
+        } else {
+            this.y--;
+        }
     }
 }
--- The Random Walker/sketch.js
+++ The Random Walker/sketch.js
@@ -1,9 +1,24 @@
+let walkers = [];
 function setup() 
 {
 	createCanvas(400, 400);
+	background(40)
+	
+	for (let i = 0; i < 15; i++) {
+		let walker = new Walker((width/2) , (height/2));
+		walkers.push(walker);
+	}
+	
 }
 
 function draw()
 {
-	background(40)
+
+	for (let i = 0; i < 15; i++) {
+
+		walkers[i].show()
+		walkers[i].step()
+
+	}
+	
 }
